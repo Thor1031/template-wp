4b651a5151493d6395d8bdd99b6bf36cb92f80e8
Thor1031 <ironman10310243@gmail.com>
2019-10-31 13:26:47 +0900

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,45 @@
-
-require("es6-promise").polyfill();
-var { src, dest, task, watch, series } = require( 'gulp' );
-var sass = require( 'gulp-sass' );
-var plumber = require( 'gulp-plumber' );
-var notify = require( 'gulp-notify' );
-var sassGlob = require( 'gulp-sass-glob' );
-var mmq = require( 'gulp-merge-media-queries' );
-var browserSync = require( 'browser-sync' );
+var { src, dest, task, watch, series } = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
+var sassGlob = require("gulp-sass-glob");
+var mmq = require("gulp-merge-media-queries");
+var browserSync = require("browser-sync");
 var reload = browserSync.reload;
 
-var imagemin = require( 'gulp-imagemin' );
-var imageminPngquant = require( 'imagemin-pngquant' );
-var imageminMozjpeg = require( 'imagemin-mozjpeg' );
+var imagemin = require("gulp-imagemin");
+var imageminPngquant = require("imagemin-pngquant");
+var imageminMozjpeg = require("imagemin-mozjpeg");
 
-var postcss = require( 'gulp-postcss' );
-var autoprefixer = require( 'autoprefixer' );
-var cssdeclsort = require( 'css-declaration-sorter' );
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssdeclsort = require("css-declaration-sorter");
 
-var ejs = require( 'gulp-ejs' );
-var rename = require( 'gulp-rename' );
-var replace = require('gulp-replace');
-var header = require('gulp-header');
-var fs = require('fs');
-var concat = require("gulp-concat");
+var ejs = require("gulp-ejs");
+var rename = require("gulp-rename");
+var replace = require("gulp-replace");
+// var header = require('gulp-header');
+var fs = require("fs");
 // var merge = require('merge-stream');
-
-var banner = [
-  '/*',
-  ' Theme Name: sample1',
-  '*/',
-  ''
-].join('\n\n');
 
 // 画像圧縮のオプション設定
 var imageminOption = [
-  imageminPngquant({ quality: '65-80' }),
-  imageminMozjpeg({ quality: 85 }),
+	imageminPngquant({ quality: "65-80" }),
+	imageminMozjpeg({ quality: 85 }),
 
-  imagemin.gifsicle({ // gif　圧縮
-    interlaced: false,
-    optimizationLevel: 1,
-    colors: 256
-  }),
-  imagemin.jpegtran(), // jpeg　圧縮
-  imagemin.optipng(), // png　圧縮
-  imagemin.svgo() // svg　圧縮
+	imagemin.gifsicle({
+		// gif　圧縮
+		interlaced: false,
+		optimizationLevel: 1,
+		colors: 256
+	}),
+	imagemin.jpegtran(), // jpeg　圧縮
+	imagemin.optipng(), // png　圧縮
+	imagemin.svgo() // svg　圧縮
 ];
 
 // 画像圧縮処理
-task('imagemin', function () {
+task("imagemin", function() {
 	return src("./img/**/*")
 		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })) // gulp中断を防ぐ
 		.pipe(imagemin(imageminOption))
@@ -56,8 +47,8 @@ task('imagemin', function () {
 });
 
 // sassのコンパイル
-task( 'sass', function() {
-  return src("./sass/**/*.scss")
+task("sass", function() {
+	return src("./sass/**/*.scss")
 		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })) // gulp中断を防ぐ
 		.pipe(sassGlob()) // import時にsetting/** のような記述ができる
 		.pipe(sass({ outputStyle: "expanded" })) // css変換処理と、出力フォーマット形式の指定
@@ -68,20 +59,30 @@ task( 'sass', function() {
 });
 
 // ファイルの監視
-task('watch', function () {
+task("watch", function() {
+	// browserSync.init({
+	// 	files: ["./**/*.php"],
+	// 	proxy: "template.local"
+	// });
+	watch("./sass/**/*.scss", series("sass", "bs-reload"));
+	// watch('./img/**/*', series("imagemin", "bs-reload"));
+});
+
+task("browser-sync", function() {
 	browserSync.init({
-		files: ["./**/*.php"],
-		proxy: "template.local"
+		proxy: "http://sample.local/"
 	});
-	watch('./sass/**/*.scss', series('sass', reload));
-	watch('./img/**/*', series("imagemin", reload));
+});
+
+task("bs-reload", function() {
+	browserSync.reload();
 });
 
 // デフォルトタスク
-task('default', series('watch'), function () {
-	watch('./*.php', task(reload));
-	watch('./css/*.css', task(reload));
-	watch('./js/*.js', task(reload));
+task("default", series("watch"), function() {
+	watch("./*.php", task("bs-reload"));
+	watch("./css/*.css", task("bs-reload"));
+	watch("./js/*.js", task("bs-reload"));
 });
 
 // サーバーの立ち上げ
